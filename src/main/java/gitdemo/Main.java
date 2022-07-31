0711c19b96329bf88a189511e6b3a65d9ca56219
subhankm <mabusubhan.k@gmail.com>
2022-07-31 18:54:36 +0530

    Git second commit to dev2

--- src/main/java/gitdemo/Main.java
+++ src/main/java/gitdemo/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Git demo");
-		System.out.println("Gi second commit to dev");
+		System.out.println("Git second commit to dev");
+		System.out.println("Git second commit to dev2");
 
 	}
 
